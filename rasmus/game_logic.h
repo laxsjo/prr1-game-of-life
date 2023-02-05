d27ffbaac90edd58b21d1caf2f50f5de3fc9b430
laxsjo <rasmus.soderhielm@gmail.com>
2023-02-05 16:42:42 +0100

    add docs to some game logic functions

--- rasmus/game_logic.h
+++ rasmus/game_logic.h
@@ -7,8 +7,12 @@ bool pointInsideBoard(const BoardState *state, const Vec2 point);
 
 bool takeInputs(BoardState *state, bool allowEdit);
 
+/// Change the size of board.
+/// Clips the board content, or pads it with zeros as necessary.
 void resizeBoard(BoardState *state, Vec2 newSize);
 
+/// Start editor, allowing user to edit board.
+/// When this function returns, state is ready to be used for the simulation.
 void startEditor(BoardState *state, char *boardName);
 
 #endif
